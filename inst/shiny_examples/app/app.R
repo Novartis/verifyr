a8e1e1234e19276ff0b4d8721fd46ffcb969c593
Juha Javanainen <javanainen@mdpi.com>
2023-12-17 16:21:40 +0100

    changed layout to exclude the sidebar + added file selectors

--- inst/shiny_examples/app/app.R
+++ inst/shiny_examples/app/app.R
@@ -4,77 +4,108 @@
 #'
 #'
 
+custom_file_input <- function (input_id, label, value = "", ...) {
+  div(class = "form-group shiny-input-container", style = "padding-right: 50px; position: relative; width: 100%;",
+    shinyFiles::shinyDirButton(paste0(input_id, "_directory"), NULL, "Please select a folder", style = "position: absolute; right: 0px; top: 25px;", icon = icon("folder-open")),
+    tags$label(label, `for` = input_id, class = "control-label"),
+    tags$input(id = input_id, type = "text", class = "shiny-input-text form-control shiny-bound-input", value = value, ...),
+  )
+}
+
 ui <- shiny::fluidPage(
   shiny::headerPanel("File Location Input"),
-  shiny::sidebarPanel(
-    shiny::textInput("old_file_location", "Old File Folder", paste0(system.file("/extdata/base_files", package = "verifyr"))),
-    shiny::textInput("new_file_location", "New File Folder", paste0(system.file("/extdata/compare_files", package = "verifyr"))),
-    shiny::textInput("file_name_patter", "File Name Pattern"),
-    shiny::textInput("omit_rows", "Omit rows with text", "Source:"),
-    shiny::actionButton("go", "Go")
+  shiny::wellPanel(
+    shiny::fluidRow(
+      shiny::column(6,
+        custom_file_input("old_file_location", "Old File Folder", system.file("/extdata/base_files", package = "verifyr")),
+        custom_file_input("new_file_location", "New File Folder", system.file("/extdata/compare_files", package = "verifyr")),
+        shiny::actionButton("go", "Go")
+      ),
+      shiny::column(6,
+        shiny::textInput("file_name_patter", "File Name Pattern"),
+        shiny::textInput("omit_rows", "Omit rows with text", "Source:"),
+      ),
+    ),
   ),
-  shiny::mainPanel(
-    h4("Initial comparison (using function initial_comparison):"),
-    DT::dataTableOutput("initial_out"),
-    h4("Side-by side comparison (using function full_comparison):"),
-    shiny::htmlOutput("full_out")
-
-  )
+  shiny::fluidRow(
+    shiny::column(12,
+      h2("Initial comparison (using function initial_comparison):"),
+      DT::dataTableOutput("initial_out"),
+    ),
+    shiny::column(12,
+      h2("Side-by side comparison (using function full_comparison):"),
+      shiny::htmlOutput("full_out")
+    ),
+  ),
+  style = "padding: 20px;",
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
 
+  roots <- c(Home = fs::path_home(), Examples = fs::path_package("verifyr", "extdata"))
 
-list_of_files <- eventReactive(input$go, {
+  shinyFiles::shinyDirChoose(input, "old_file_location_directory", roots = roots, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
+  shinyFiles::shinyDirChoose(input, "new_file_location_directory", roots = roots, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
+
+  observe({
+    if (!is.integer(input$old_file_location_directory)) {
+      shiny::updateTextInput(session, "old_file_location",
+        label <- NULL,
+        value <- parseDirPath(roots, input$old_file_location_directory)
+      )
+    }
+
+    if (!is.integer(input$new_file_location_directory)) {
+      shiny::updateTextInput(session, "new_file_location",
+        label <- NULL,
+        value <- parseDirPath(roots, input$new_file_location_directory)
+      )
+    }
+  })
+
+  list_of_files <- shiny::eventReactive(input$go, {
     if (file.exists(input$old_file_location) && file.exists(input$new_file_location)) {
       verifyr::list_files(input$old_file_location, input$new_file_location, input$file_name_patter)
     }
   })
 
-initial_verify <- shiny::reactive({
-  if (!is.null(list_of_files())) {
-    tibble::tibble(list_of_files()) %>%
-    dplyr::mutate(omitted = input$omit_rows) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(comparison = verifyr::initial_comparison(old =old_path,new=new_path, omit = omitted))
-  }
-})
-
-full_verify <- shiny::reactive({
-  if (file.exists(input$old_file_location) && file.exists(input$new_file_location)) {
-    verifyr::full_comparison(input$old_file_location, input$new_file_location)
-  }
-})
-
-
-output$initial_out <-  DT::renderDataTable({
-  if (!is.null(initial_verify())) {
-    DT::datatable(initial_verify(), selection = "single")
-  } else {
-    "No folder selected or folders do not exist"
-  }
-})
-
-
-
-shiny::observe({
-  shiny::req(input$initial_out_rows_selected)
-  selRow <- initial_verify()[input$initial_out_rows_selected,]
-  output$full_out <- shiny::renderUI({
-
-    #list side-by-side comparison
-    shiny::HTML(
-      as.character(
-
-        verifyr::full_comparison(paste0(selRow[2]), paste0(selRow[3]))
-      )
-    )
-
+  initial_verify <- shiny::reactive({
+    if (!is.null(list_of_files())) {
+      tibble::tibble(list_of_files()) %>%
+      dplyr::mutate(omitted = input$omit_rows) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(comparison = verifyr::initial_comparison(old =old_path,new=new_path, omit = omitted))
+    }
   })
 
-})
+  full_verify <- shiny::reactive({
+    if (file.exists(input$old_file_location) && file.exists(input$new_file_location)) {
+      verifyr::full_comparison(input$old_file_location, input$new_file_location)
+    }
+  })
 
+  output$initial_out <-  DT::renderDataTable({
+    if (!is.null(initial_verify())) {
+      DT::datatable(initial_verify(), selection = "single")
+    } else {
+      "No folder selected or folders do not exist"
+    }
+  })
 
+  shiny::observe({
+    shiny::req(input$initial_out_rows_selected)
+    selRow <- initial_verify()[input$initial_out_rows_selected,]
+
+    output$full_out <- shiny::renderUI({
+
+    #list side-by-side comparison
+      shiny::HTML(
+        as.character(
+          verifyr::full_comparison(paste0(selRow[2]), paste0(selRow[3]))
+        )
+      )
+    })
+  })
 }
 
 shiny::shinyApp(ui, server)
