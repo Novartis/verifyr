7b1223b481c9b5f7c1d5f28ca61da0b9695d0888
Juha Javanainen <javanainen@mdpi.com>
2023-12-18 11:46:49 +0100

    cleaning up the shiny app changes and fixing the lint issues

--- inst/shiny_examples/app/app.R
+++ inst/shiny_examples/app/app.R
@@ -4,11 +4,15 @@
 #'
 #'
 
-custom_file_input <- function (input_id, label, value = "", ...) {
-  div(class = "form-group form-group-custom shiny-input-container",
-    shinyFiles::shinyDirButton(paste0(input_id, "_directory"), NULL, "Please select a folder", icon = icon("folder-open")),
-    tags$label(label, `for` = input_id, class = "control-label"),
-    tags$input(id = input_id, type = "text", class = "shiny-input-text form-control shiny-bound-input", value = value, ...),
+custom_file_input <- function(input_id, label, value = "", ...) {
+  label_class  <- "control-label"
+  input_class  <- "shiny-input-text form-control shiny-bound-input"
+  folder_value <- system.file(value, package = "verifyr")
+
+  shiny::div(class = "form-group form-group-custom shiny-input-container",
+    shinyFiles::shinyDirButton(paste0(input_id, "_select"), NULL, "Select folder", icon = shiny::icon("folder-open")),
+    tags$label(label, `for` = input_id, class = label_class),
+    tags$input(id = input_id, type = "text", class = input_class, value = folder_value, ...),
   )
 }
 
@@ -18,8 +22,8 @@ ui <- shiny::fluidPage(
   shiny::wellPanel(
     shiny::fluidRow(
       shiny::column(6,
-        custom_file_input("old_file_location", "Old File Folder", system.file("/extdata/base_files", package = "verifyr")),
-        custom_file_input("new_file_location", "New File Folder", system.file("/extdata/compare_files", package = "verifyr")),
+        custom_file_input("old_folder", "Old File Folder", "/extdata/base_files"),
+        custom_file_input("new_folder", "New File Folder", "/extdata/compare_files"),
         shiny::actionButton("go", "Go")
       ),
       shiny::column(6,
@@ -43,49 +47,47 @@ ui <- shiny::fluidPage(
 )
 server <- function(input, output, session) {
 
-  roots <- c(Home = fs::path_home(), Examples = fs::path_package("verifyr", "extdata"))
+  roots  <- c(Home = fs::path_home(), Examples = fs::path_package("verifyr", "extdata"))
+  params <- list(roots = roots, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
 
-  shinyFiles::shinyDirChoose(input, "old_file_location_directory", roots = roots, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
-  shinyFiles::shinyDirChoose(input, "new_file_location_directory", roots = roots, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
+  do.call(shinyFiles::shinyDirChoose, c(list(input, "old_folder_select"), params))
+  do.call(shinyFiles::shinyDirChoose, c(list(input, "new_folder_select"), params))
 
-  initial_out_placeholder_text <- reactiveVal('Select the compared file folders and execute the initial comparison by clicking on the "Go" button.')
-  full_out_placeholder_text    <- reactiveVal('Click on a row in the initial comparison result to view the detailed side-by-side comparison.')
+  default1 <- "Select the compared file folders and execute the initial comparison by clicking on the 'Go' button."
+  initial_out_placeholder_text <- shiny::reactiveVal(default1)
+
+  default2 <- "Click on a row in the initial comparison result to view the detailed side-by-side comparison."
+  full_out_placeholder_text <- shiny::reactiveVal(default2)
 
   shiny::observe({
-    if (!is.integer(input$old_file_location_directory)) {
-      shiny::updateTextInput(session, "old_file_location",
-        label <- NULL,
-        value <- parseDirPath(roots, input$old_file_location_directory)
+    if (!is.integer(input$old_folder_select)) {
+      shiny::updateTextInput(session, "old_folder",
+        NULL,
+        shinyFiles::parseDirPath(roots, input$old_folder_select)
       )
     }
 
-    if (!is.integer(input$new_file_location_directory)) {
-      shiny::updateTextInput(session, "new_file_location",
-        label <- NULL,
-        value <- shinyFiles::parseDirPath(roots, input$new_file_location_directory)
+    if (!is.integer(input$new_folder_select)) {
+      shiny::updateTextInput(session, "new_folder",
+        NULL,
+        shinyFiles::parseDirPath(roots, input$new_folder_select)
       )
     }
   })
 
   list_of_files <- shiny::eventReactive(input$go, {
-    if (file.exists(input$old_file_location) && file.exists(input$new_file_location)) {
-      initial_out_placeholder_text('') 
-      verifyr::list_files(input$old_file_location, input$new_file_location, input$file_name_patter)
+    if (file.exists(input$old_folder) && file.exists(input$new_folder)) {
+      initial_out_placeholder_text("")
+      verifyr::list_files(input$old_folder, input$new_folder, input$file_name_patter)
     }
   })
 
   initial_verify <- shiny::reactive({
     if (!is.null(list_of_files())) {
       tibble::tibble(list_of_files()) %>%
-      dplyr::mutate(omitted = input$omit_rows) %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(comparison = verifyr::initial_comparison(old =old_path,new=new_path, omit = omitted))
-    }
-  })
-
-  full_verify <- shiny::reactive({
-    if (file.exists(input$old_file_location) && file.exists(input$new_file_location)) {
-      verifyr::full_comparison(input$old_file_location, input$new_file_location)
+        dplyr::mutate(omitted = input$omit_rows) %>%
+        dplyr::rowwise() %>%
+        dplyr::mutate(comparison = verifyr::initial_comparison(old =old_path,new=new_path, omit = omitted)) # nolint
     }
   })
 
@@ -99,22 +101,27 @@ server <- function(input, output, session) {
 
   shiny::observe({
     shiny::req(input$initial_out_rows_selected)
-    selRow <- initial_verify()[input$initial_out_rows_selected,]
-    full_out_placeholder_text('') 
+    sel_row <- initial_verify()[input$initial_out_rows_selected, ]
+    full_out_placeholder_text("")
 
     #list side-by-side comparison
     output$full_out <- shiny::renderUI({
       shiny::HTML(
         as.character(
-          verifyr::full_comparison(paste0(selRow[2]), paste0(selRow[3]))
+          verifyr::full_comparison(paste0(sel_row[2]), paste0(sel_row[3]))
         )
       )
     })
   })
 
   # set up the reactive value bindings to default outputs
-  output$initial_out_placeholder <- renderText({ initial_out_placeholder_text() })
-  output$full_out_placeholder <- renderText({ full_out_placeholder_text() })
+  output$initial_out_placeholder <- shiny::renderText({
+    initial_out_placeholder_text()
+  })
+
+  output$full_out_placeholder <- shiny::renderText({
+    full_out_placeholder_text()
+  })
 }
 
 shiny::shinyApp(ui, server)
